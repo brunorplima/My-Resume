7b2debc361655854a07fea3561b7558504371d9e
brunorplima <bruno.reactdeveloper@gmail.com>
2020-03-07 08:15:09 -0800

    Added emailJS service to contact section

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,23 +1,68 @@
 import React, { useState } from 'react';
 import { FaLinkedin, FaFacebookSquare } from 'react-icons/fa';
-import { TiSocialInstagram } from 'react-icons/ti'
+import { TiSocialInstagram } from 'react-icons/ti';
 
 import './contact.scss'
 
+const _status = {
+   inactive: 'inactive',
+   sent: 'sent',
+   invalid: 'invalid',
+   error: 'error'
+}
+Object.freeze(_status);
+
 const Contact = () => {
 
-   const [name, setName] = useState('')
-   const [email, setEmail] = useState('')
-   const [subject, setSubject] = useState('')
-   const [message, setMessage] = useState('')
+   const [name, setName] = useState('');
+   const [email, setEmail] = useState('');
+   const [subject, setSubject] = useState('');
+   const [message, setMessage] = useState('');
+   const [status, setStatus] = useState(_status.inactive);
+   const [error, setError] = useState('');
    
    const handleSendButton = (e) => {
       e.preventDefault();
+
       if(name && email && subject && message) {
-         console.log('Approved');
+         const params = {
+            name: name,
+            to_email: email,
+            subject: subject,
+            message: message
+            }
+         window.emailjs.send('default_service', 'contact_my_resume', params)
+            .then(response => {
+               setStatus(_status.sent);
+               setName('');
+               setEmail('');
+               setSubject('');
+               setMessage('');
+               resetStatus(10000);
+            }, 
+            error => {
+               console.log('Error: ' + error);
+               setStatus(_status.error);
+               setError(error);
+               resetStatus(25000);
+            })
       } else {
-         console.log('disapproved');
+         console.log('There is invalid value in the form');
+         setStatus(_status.invalid);
+         resetStatus(25000);
       }
+
+      // if (status !== _status.inactive) {
+      //    setTimeout(() => {
+      //       setStatus(_status.inactive);
+      //    }, 2000);
+      // }
+   }
+
+   const resetStatus = time => {
+      setTimeout(() => {
+         setStatus(_status.inactive);
+      }, time);
    }
 
    return (
@@ -25,12 +70,32 @@ const Contact = () => {
          <div className="contact-form d-flex justify-content-center align-items-center">
             <form className="d-flex flex-column">
                <div className="d-flex justify-content-between">
-                  <input type="input" name="name" placeholder="Name" value={name} onChange={e => setName(e.target.value)} required/>
-                  <input type="input" name="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} required/>
+                  <input type="input" name="name" placeholder="Name" value={name} onChange={e => setName(e.target.value)} style={!name && status === 'invalid' ? {border: '1.5px solid red'} : {}}/>
+                  <input type="input" name="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} style={!email && status === 'invalid' ? {border: '1.5px solid red'} : {}}/>
                </div>
-               <input type="input" name="subject" placeholder="Subject" value={subject} onChange={e => setSubject(e.target.value)} required/>
-               <textarea name="message" placeholder="Message" value={message} onChange={e => setMessage(e.target.value)} required></textarea>
+               <input type="input" name="subject" placeholder="Subject" value={subject} onChange={e => setSubject(e.target.value)} style={!subject && status === 'invalid' ? {border: '1.5px solid red'} : {}}/>
+               <textarea name="message" placeholder="Message" value={message} onChange={e => setMessage(e.target.value)} style={!message && status === 'invalid' ? {border: '1.5px solid red'} : {}}></textarea>
                <button onClick={(e) => handleSendButton(e)}>Send</button>
+               
+               {status === _status.sent ?
+                  <div className="form-feedback email-sent">
+                     <span>Thank you! Message sent.</span>
+                  </div> :
+                  null
+               }
+               {status === _status.invalid ?
+                  <div className="form-feedback email-invalid">
+                     <span>All fields are required.</span>
+                  </div> :
+                  null
+               }
+               {status === _status.error ?
+                  <div className="form-feedback email-invalid">
+                     <span>Error: {error}.</span>
+                  </div> :
+                  null
+               }
+
             </form>
          </div>
 
