c44b32128570d9b0f0273e475fe11cd1396db0a2
brunorplima <bruno.reactdeveloper@gmail.com>
2020-03-16 07:08:28 -0700

    Added and styled github link

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import iplogo from '../../images/iplogo.gif';
-import { FaChevronDown } from 'react-icons/fa';
 
 import './portfolio.scss';
 
@@ -8,14 +7,14 @@ const Portfolio = () => {
    return (
       <div className="portfolio-container d-flex justify-content-around align-items-center flex-column  flex-md-row">
          <div>
-            <a href="https://inheritance-publications.web.app/" target="_blank" className="work-container d-flex flex-column align-items-center">
+            <a href="https://inheritance-publications.web.app/" target="_blank" rel="noopener noreferrer" className="work-container d-flex flex-column align-items-center">
                <img src={iplogo} alt="IP logo" className="ip-logo"/>
                <h4>Inheritance Publications</h4>
                <span>Ecommerce created for Inheritance Publication Inc. to improve the user experience and online presence of the company in order to increase sales</span>
             </a>
-            <div className="project-details text-center">
+            <a className="project-details text-center d-flex justify-content-center" href="https://github.com/brunorplima/InhPubl" target="_blank" rel="noopener noreferrer">
                <span>Project Details - GitHub</span>
-            </div>
+            </a>
          </div>
 
          {/* <div className="work-container d-flex flex-column align-items-center">
