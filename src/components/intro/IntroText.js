6a6add4b1cd1e77c155fb24ef3e427ba515e9b84
brunorplima <bruno.reactdeveloper@gmail.com>
2020-03-18 07:39:04 -0700

    Replaced lorem ipsum with real content

--- src/components/intro/IntroText.js
+++ src/components/intro/IntroText.js
@@ -5,7 +5,7 @@ import './intro-text.scss';
 const IntroText = () => {
    return (
       <div className="intro-text d-flex justify-content-center align-items-center ps-1">
-         <p>"Hello, I'm Bruno. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet purus gravida quis blandit turpis"</p>
+         <p>Graduated on software and web development, acquired interest and experience working with front-end technologies such as HTML/CSS, Javascript and React. Seeking a position as a front-end web/app development using libraries/frameworks like React and React Native, aiming the leverage of problem solving, logic, design and communication skills for mutual benefit.</p>
       </div>
    )
 }
