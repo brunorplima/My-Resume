0ed15717d7994debdd8edae4895408010e807779
Bruno Lima <brunolima.cad@gmail.com>
2020-09-24 05:58:09 -0600

    improved style

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Skill from './Skill';
+import { TiInputChecked } from 'react-icons/ti';
 
 import './skills.scss';
 
@@ -8,35 +9,30 @@ const Skills = () => {
       <div className="skills">
          <div className="skills-intro d-flex flex-column align-items-center">
             <p className="skills-text">
-               Ability to work in a team using personal and digital means to maintain communication, Design of web and mobile apps using various available tools to enhance and test user experience. Use of Javascript frameworks and libraries to give life to those designs.
+               Ability to work in a team using personal and digital means to maintain communication, Design and development of web apps using various available tools to enhance and test user experience. Development using Javascript and React to give life to those designs.
             </p>
-            <ul className="d-flex">
-               <div>
-                  <li>Quick learner</li>
-                  <li>Passionate about programming and UI development</li>
-                  <li>Hard worker</li>
-                  <li>Good communication</li>
-               </div>
-               <div>
-                  <li>Disciplined</li>
-                  <li>Organized</li>
-                  <li>Problem solver</li>
-                  <li>Detail oriented</li>
-               </div>
-            </ul>
+            <div className="list d-flex">
+               <div className='list-item'><div><TiInputChecked /></div> <div>Quick learner</div></div>
+               <div className='list-item'><div><TiInputChecked /></div> <div>Passionate about programming</div></div>
+               <div className='list-item'><div><TiInputChecked /></div> <div>Hard worker</div></div>
+               <div className='list-item'><div><TiInputChecked /></div> <div>Good communication</div></div>
+               <div className='list-item'><div><TiInputChecked /></div> <div>Disciplined</div></div>
+               <div className='list-item'><div><TiInputChecked /></div> <div>Organized</div></div>
+               <div className='list-item'><div><TiInputChecked /></div> <div>Problem solver</div></div>
+               <div className='list-item'><div><TiInputChecked /></div> <div>Detail oriented</div></div>
+            </div>
          </div>
          
          <div className="d-flex justify-content-center">
             <div className="skills-container">
-               <Skill skill="Web Design" width="50%" time="2 years"/>
-               <Skill skill="HTML" width="80%" time="3 years"/>
-               <Skill skill="CSS/SASS" width="70%" time="3 years"/>
-               <Skill skill="Javascript/ES6" width="80%" time="1 year"/>
-               <Skill skill="OO Programming" width="85%" time="3 year"/>
-               <Skill skill="SQL/NoSQL" width="70%" time="2 year"/>
-               <Skill skill="React" width="85%" time="7 months"/>
+               <Skill skill="Web Design" width="60%" />
+               <Skill skill="HTML" width="80%" />
+               <Skill skill="CSS/SASS" width="70%" />
+               <Skill skill="Javascript/ES6" width="80%" />
+               <Skill skill="OO Programming" width="75%" />
+               <Skill skill="SQL/NoSQL" width="70%" />
+               <Skill skill="React" width="90%" />
                <Skill skill="Git" width="50%" />
-               <span style={{color:'#aaa', fontSize:'.8rem'}}>Note: experience here means academic experience</span>
             </div>
          </div>
       </div>
