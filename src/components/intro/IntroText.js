18c8f0264fa33cb0803769ff381d53492a9bf988
Bruno Lima <brunolima.cad@gmail.com>
2020-09-24 03:16:02 -0600

    text changed

--- src/components/intro/IntroText.js
+++ src/components/intro/IntroText.js
@@ -5,7 +5,7 @@ import './intro-text.scss';
 const IntroText = () => {
    return (
       <div className="intro-text d-flex justify-content-center align-items-center ps-1">
-         <p>Graduated on software and web development, acquired interest and experience working with front-end technologies such as HTML/CSS, Javascript and React. Seeking a position as a front-end web/app development using libraries/frameworks like React and React Native, aiming the leverage of problem solving, logic, design and communication skills for mutual benefit.</p>
+         <p>Recently graduated on software and web development, acquired interest and experience working with front-end technologies such as HTML, CSS, Javascript and React. Seeking a position as a front-end web and app developer using libraries like React and React Native, aiming the leverage of problem solving, logic, design and communication skills for mutual benefit.</p>
       </div>
    )
 }
