1e012f07791407e3ece0f2dce01d7233d68416bf
Bruno Lima <brunolima.cad@gmail.com>
2020-09-26 06:33:23 -0600

    added new component Project

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,27 +1,52 @@
 import React from 'react';
-import iplogo from '../../images/iplogo.gif';
+import Project from './Project'
+import iplogo from '../../images/iplogo-bg.PNG';
+import allan from '../../images/allan-project.PNG';
+import minesweeper from '../../images/minesweeper.PNG';
 
 import './portfolio.scss';
 
 const Portfolio = () => {
    return (
-      <div className="portfolio-container d-flex justify-content-around align-items-center flex-column  flex-md-row">
-         <div>
-            <a href="https://inheritance-publications.web.app/" target="_blank" rel="noopener noreferrer" className="work-container d-flex flex-column align-items-center">
-               <img src={iplogo} alt="IP logo" className="ip-logo"/>
-               <h4>Inheritance Publications</h4>
-               <span>Ecommerce created for Inheritance Publication Inc. to improve the user experience and online presence of the company in order to increase sales</span>
-            </a>
-            <a className="project-details text-center d-flex justify-content-center" href="https://github.com/brunorplima/InhPubl" target="_blank" rel="noopener noreferrer">
-               <span>Project Details - GitHub</span>
-            </a>
-         </div>
+      <div className="portfolio-container d-flex justify-content-center align-items-center">
+         <div className='portfolio d-flex flex-wrap justify-content-around'>
+            <Project
+               img={iplogo}
+               bgSize='18rem'
+               description='Online Book Store for Inheritance Publications'
+               layerColor={'#rgba(26,83,92,.3)'}
+               info={{
+                  name: 'Inheritance Publications Online Store',
+                  summary: 'This project was a free work that eventually led me to work for the Inheritance Publication as a webmaster',
+                  gitLink: 'http://um_link.com',
+                  link: 'http://inhpubl.net',
+               }}
+            />
 
-         {/* <div className="work-container d-flex flex-column align-items-center">
-            <img src={app} alt="IP logo" className="app-logo"/>
-             <h4>BBudget App</h4>
-             <span>An iOS and Android app that records user’s incomes and expenses keeping track of the user’s financial situation</span>
-         </div> */}
+            <Project 
+               img={allan} 
+               bgSize='17rem'
+               description="My son's online baby shower"
+               info={{
+                  name: "Allan's Baby Shower",
+                  summary: "In times of COVID-19 I had the idea of celebrating my son's baby shower online. I decided to allow some interaction from our guests (users) within the page",
+                  gitLink: 'http://um_link.com',
+                  link: 'https://allanestachegando.web.app',
+               }}
+            />
+
+            <Project 
+               img={minesweeper} 
+               bgSize='17rem'
+               description='Minesweeper game'
+               info={{
+                  name: 'Minesweeper',
+                  summary: 'Minesweeper is an interesting game. Sometime in my life I started to enjoy playing it. Then I decided to develope a nice one.',
+                  gitLink: 'http://um_link.com',
+                  link: 'https://brunosminesweeper.web.app',
+               }}
+            />
+         </div>
       </div>
    )
 }
