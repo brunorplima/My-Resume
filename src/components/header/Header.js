34ce85c4e09ea81b91ac9f3f1a31c666ffda3c87
Bruno Lima <brunolima.cad@gmail.com>
2020-09-26 06:22:50 -0600

    animation; better ux

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -9,15 +9,38 @@ const Header = () => {
 
    const [position, setPosition] = useState(0);
    const [menuPressed, setMenuPressed] = useState(false);
+   const [style, setStyle] = useState({});
 
    window.addEventListener('scroll', () => {
       setPosition(window.scrollY)
    })
 
+   function clickLink() {
+      if (menuPressed) {
+
+      }
+   }
+
+   function clickMenu() {
+      if (menuPressed) {
+         setStyle({
+            transform: 'translateX(60rem)',
+            animationName: 'slide-out'
+         });
+         setMenuPressed(false);
+      } else {
+         setStyle({
+            transform: 'translateX(0rem)',
+            animationName: 'slide-in'
+         })
+         setMenuPressed(true);
+      }
+   }
+
    return (
       <header id="navbar" className="container-fluid" style={position <= 300 ? {padding: '.8rem 0'} : {}}>
          <div className="row justify-content-between">
-            <div className="col-1 offset-1">
+            <div className="div-img col-1 offset-1">
                <img src={logo} alt="Bruno Lima logo" style={position > 300 ? {width: '2rem'} : {}}/>
             </div>
 
@@ -31,7 +54,7 @@ const Header = () => {
                </nav>
 
                <div className="menu-icon-container d-flex d-md-none justify-content-end">
-                  <div className="menu-icon" style={position >= 300 ? {fontSize: '23pt', paddingRight: 0} : {}} onClick={() => setMenuPressed(!menuPressed)}>
+                  <div className="menu-icon" style={position >= 300 ? {fontSize: '23pt', paddingRight: 0} : {}} onClick={clickMenu}>
                      {menuPressed ?
                         <IoMdClose /> :
                         <IoIosMenu />
@@ -41,20 +64,15 @@ const Header = () => {
             </div>
          </div>
 
-         {menuPressed ?
-         
-            <div className="nav-screen" style={position >= 300 ? {left: 0} : {}}>
-               <nav className="d-flex flex-column align-items-center">
-                  <Link to="intro" smooth={true} duration={600} className="router-link" onClick={() => setMenuPressed(!menuPressed)}>INTRO</Link>
-                  <Link to="education" smooth={true} duration={700} className="router-link" onClick={() => setMenuPressed(!menuPressed)}>EDUCATION</Link>
-                  <Link to="portfolio" smooth={true} duration={800} className="router-link" onClick={() => setMenuPressed(!menuPressed)}>PORTFOLIO</Link>
-                  <Link to="skills" smooth={true} duration={900} className="router-link" onClick={() => setMenuPressed(!menuPressed)}>SKILLS</Link>
-                  <Link to="contact" smooth={true} duration={1000} className="router-link" onClick={() => setMenuPressed(!menuPressed)}>CONTACT</Link>
-               </nav>
-            </div>
-             :
-            null
-         }
+         <div className="nav-screen" style={style}>
+            <nav className="d-flex flex-column align-items-center">
+               <Link to="intro" smooth={true} duration={600} className="router-link" onClick={() => clickMenu()}>INTRO</Link>
+               <Link to="education" smooth={true} duration={700} className="router-link" onClick={() => clickMenu()}>EDUCATION</Link>
+               <Link to="portfolio" smooth={true} duration={800} className="router-link" onClick={() => clickMenu()}>PORTFOLIO</Link>
+               <Link to="skills" smooth={true} duration={900} className="router-link" onClick={() => clickMenu()}>SKILLS</Link>
+               <Link to="contact" smooth={true} duration={1000} className="router-link" onClick={() => clickMenu()}>CONTACT</Link>
+            </nav>
+         </div>
       </header>
    )
 }
