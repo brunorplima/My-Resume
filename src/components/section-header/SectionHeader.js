3661d917dbe6fb0c9fc8da46111560bc727de599
Bruno Lima <brunolima.cad@gmail.com>
2020-12-21 23:32:49 -0700

    add text-shadow, changed font, allow props for bg color

--- src/components/section-header/SectionHeader.js
+++ src/components/section-header/SectionHeader.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import './section-header.scss';
 
-const SectionHeader = props => {
+const SectionHeader = ({ id, title, color }) => {
    return (
-      <div id={props.id} className="sh-container d-flex justify-content-center p-4">
-         <h2>{props.title.toUpperCase()}</h2>         
+      <div className="sh-container d-flex justify-content-center p-4" style={color ? {backgroundColor: color} : {}}>
+         <div id={id} className='scroll-to'></div>
+         <h2>{title.toUpperCase()}</h2>
       </div>
    )
 }
