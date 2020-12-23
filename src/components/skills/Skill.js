c92e57fc200a409acf1f2a6de69fe2a846c88a12
Bruno Lima <brunolima.cad@gmail.com>
2020-12-23 01:56:22 -0700

    improved style and added animation

--- src/components/skills/Skill.js
+++ src/components/skills/Skill.js
@@ -1,16 +1,75 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import { SiCss3 } from 'react-icons/si';
+import { SiHtml5 } from 'react-icons/si';
+import { SiJavascript } from 'react-icons/si';
+import { FaDatabase } from 'react-icons/fa';
+import { FaReact } from 'react-icons/fa';
+import { DiGit } from 'react-icons/di';
+
 
 import './skill.scss'
 
-const Skill = props => {
+const Skill = ({ skill, image, bgColor, icon, iconColor, fontSize, isLast, level }) => {
+
+   const container = useRef();
+   const [animationClass, setAnimationClass] = useState('');
+
+   useEffect(() => {
+      document.addEventListener('scroll', scrollEventFunction);
+   }, []);
+
+   function scrollEventFunction() {
+      const clientRect = container.current.getBoundingClientRect();
+      const viewportEnd = window.innerHeight || document.documentElement.clientHeight;
+      if (clientRect.top >= 0 && clientRect.top <= viewportEnd) {
+         setAnimationClass('fill-blank-anim');
+         document.removeEventListener('scroll', scrollEventFunction);
+      }
+   }
+
+   function getIcon() {
+      switch (icon) {
+         case 'css':
+            return <SiCss3 />
+         case 'html':
+            return <SiHtml5 />;
+         case 'js':
+            return <SiJavascript />
+         case 'nosql':
+            return <FaDatabase />;
+         case 'react':
+            return <FaReact />;
+         case 'git':
+            return <DiGit />;
+         default:
+            console.error('getIcon() should not get to default!');
+            return null;
+      }
+   }
+
    return (
-      <div className="skill d-flex justify-content-between align-items-center">
-         <span>{props.skill}</span>
-         <div className="percent-bg">
-            <div className="percent-actual" style={{width: props.width}}></div>
+      <div ref={container} className='dv-skill-container d-flex' style={isLast ? { marginBottom: 0 } : {}}>
+         <div
+            className='dv-s-image d-flex justify-content-center align-items-center'
+            style={image ? { backgroundImage: `url(${image})`, backgroundPosition: 'center', backgroundSize: 'cover' } : { backgroundColor: bgColor }}
+         >
+            {icon && <div style={{ color: iconColor, fontSize, transform: 'translateY(-4px) translatex(-4px)' }}>{getIcon()}</div>}
+            <div className='dv-s-shadow' style={level ? {} : { background: 'linear-gradient(to left, white, rgba(255,255,255, 0))' }}></div>
+         </div>
+         <div className='dv-s-skill d-flex justify-content-center align-items-center'>
+            <div className={`dv-skill-pc ${animationClass}`} style={{ width: level }}></div>
+            <h4>{skill}</h4>
          </div>
       </div>
    )
 }
 
 export default Skill
+
+
+      // <div className="skill d-flex justify-content-between align-items-center">
+      //    <span>{props.skill}</span>
+      //    <div className="percent-bg">
+      //       <div className="percent-actual" style={{width: props.width}}></div>
+      //    </div>
+      // </div>
--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Skill from './Skill';
 import { TiInputChecked } from 'react-icons/ti';
+import webDesign from '../../images/web-design.jpg'
 
 import './skills.scss';
 
@@ -8,33 +9,39 @@ const Skills = () => {
    return (
       <div className="skills">
          <div className="skills-intro d-flex flex-column align-items-center">
-            <p className="skills-text">
-               Ability to work in a team using personal and digital means to maintain communication, Design and development of web apps using various available tools to enhance and test user experience. Development using Javascript and React to give life to those designs.
-            </p>
-            <div className="list d-flex">
+            <h3 className="skills-text">
+               Personal qualifications
+            </h3>
+            <div className="list d-flex justify-content-center">
                <div className='list-item'><div><TiInputChecked /></div> <div>Quick learner</div></div>
                <div className='list-item'><div><TiInputChecked /></div> <div>Passionate about programming</div></div>
                <div className='list-item'><div><TiInputChecked /></div> <div>Hard worker</div></div>
                <div className='list-item'><div><TiInputChecked /></div> <div>Good communication</div></div>
                <div className='list-item'><div><TiInputChecked /></div> <div>Disciplined</div></div>
-               <div className='list-item'><div><TiInputChecked /></div> <div>Organized</div></div>
                <div className='list-item'><div><TiInputChecked /></div> <div>Problem solver</div></div>
                <div className='list-item'><div><TiInputChecked /></div> <div>Detail oriented</div></div>
             </div>
          </div>
-         
-         <div className="d-flex justify-content-center">
-            <div className="skills-container">
-               <Skill skill="Web Design" width="60%" />
-               <Skill skill="HTML" width="80%" />
-               <Skill skill="CSS/SASS" width="70%" />
-               <Skill skill="Javascript/ES6" width="80%" />
-               <Skill skill="OO Programming" width="75%" />
-               <Skill skill="SQL/NoSQL" width="70%" />
-               <Skill skill="React" width="90%" />
-               <Skill skill="Git" width="50%" />
-            </div>
+
+         <br/>
+
+         <div className='d-flex justify-content-center'>
+            <h3 className="skills-text text-center">
+               Professional skills
+            </h3>
          </div>
+            <br/>
+            <div className="d-flex justify-content-center">
+               <div className="skills-container d-flex justify-content-around flex-wrap">
+                  <Skill skill="Web Design" image={webDesign} level='60%'/>
+                  <Skill skill="CSS/SCSS" icon='css' iconColor='#2965f1' bgColor='#D8D8D8' level='70%' />
+                  <Skill skill="HTML" icon='html' iconColor='#e34c26' bgColor='#FFFFEA' level='80%' />
+                  <Skill skill="Javascript/ES6" icon='js' iconColor='#f0db4f' bgColor='#333' level='85%' />
+                  <Skill skill="NoSQL" icon='nosql' iconColor='white' bgColor='#00CECB' level='70%' />
+                  <Skill skill="React" icon='react' iconColor='#61DBFB' bgColor='#333' level='90%' />
+                  <Skill skill="Git" icon='git' iconColor='#f34f29' bgColor='#5d4d20' fontSize='36pt' isLast={true} level='60%' />
+               </div>
+            </div>
       </div>
    )
 }
