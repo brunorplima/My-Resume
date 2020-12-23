984f27d617f1305afa7a1d86ee3190992a3472a8
Bruno Lima <brunolima.cad@gmail.com>
2020-12-23 01:57:27 -0700

    imported Experience component into App.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import SocialMedia from './components/social-media/SocialMedia';
 import IntroText from './components/intro/IntroText';
 import SectionHeader from './components/section-header/SectionHeader';
 import Education from './components/education/Education';
+import Experience from './components/experience/Experience';
 import Portfolio from './components/portfolio/Portfolio';
 import Skills from './components/skills/Skills';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; 
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 
 function App() {
@@ -22,16 +23,19 @@ function App() {
             <SocialMedia />
             <IntroText />
 
-            <SectionHeader title="Education" id="education"/>
+            <SectionHeader title="Education" id="education" />
             <Education />
 
-            <SectionHeader title="Portfolio" id="portfolio"/>
-            <Portfolio />
-
-            <SectionHeader title="Skills" id="skills"/>
+            <SectionHeader title="Skills" id="skills" color='#f1f1f1'/>
             <Skills />
 
-            <SectionHeader title="Contact" id="contact"/>
+            <SectionHeader title="Experience" id="experience" />
+            <Experience />
+
+            <SectionHeader title="Portfolio" id="portfolio" />
+            <Portfolio />
+
+            <SectionHeader title="Contact" id="contact" />
             <Contact />
 
             <Footer />
