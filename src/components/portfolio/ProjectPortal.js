79396174a90c661068c19bff8a92c9af585078ad
Bruno Lima <brunolima.cad@gmail.com>
2020-12-26 14:36:16 -0700

    Improved ProjectPortal component's style

--- src/components/portfolio/ProjectPortal.js
+++ src/components/portfolio/ProjectPortal.js
@@ -36,19 +36,24 @@ const ProjectPortal = ({ setIsOpen, info, img }) => {
                   className='close-icon d-flex justify-content-center align-items-center'
                   onClick={() => setIsOpen(false)}
                >
-                  <IoMdClose className='' />
+                  <IoMdClose />
                </div>
             </div>
-            <div className='content p-3'>
-               <img src={img} alt={info.name} />
-               <h2>{info.name}</h2>
-               <p>{info.summary}</p>
-               <p>
-                  {info.gitLink ?
-                     <a href={info.gitLink} target='_blank' rel="noopener noreferrer">Github repository</a> :
-                     getPrivateRepoMessage('Github repository is private due to Inheritance Publications\' sensitive information')
-                  }</p>
-               <p>Visit the project's <a href={info.link} target='_blank' rel="noopener noreferrer">website</a></p>
+            <div className='content'>
+               <div className='content-bg-img p-3 d-flex align-items-center' style={{background: `url(${img})`}}>
+                  <div className='content-bg-img-shadow'></div>
+                  <h2>{info.name}</h2>
+               </div>
+
+               <div className='p-3'>
+                  <p>{info.summary}</p>
+                  <p>
+                     {info.gitLink ?
+                        <a href={info.gitLink} target='_blank' rel="noopener noreferrer">Github repository</a> :
+                        getPrivateRepoMessage('Github repository is private due to Inheritance Publications\' sensitive information')
+                     }</p>
+                  <p>Visit the project's <a href={info.link} target='_blank' rel="noopener noreferrer">website</a></p>
+               </div>
             </div>
          </div>
       </div>,
